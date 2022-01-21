d73d83d4ff5ca56ce543435b576ae80e985709ec
gbL <gabrielhenrique2504@gmail.com>
2022-01-20 21:01:39 -0300

    Mapeamento das entidades

--- ASP.NET MVC Core 6/src/Data/ApplicationDbContext.cs
+++ ASP.NET MVC Core 6/src/Data/ApplicationDbContext.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using src.Models;
 
 namespace App.MVC.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
-    }
+        : base(options) { }
+
+    public DbSet<Produto> Produtos { get; set; }
+    public DbSet<Fornecedor> Fornecedores { get; set; }
+    public DbSet<Endereco> Enderecos { get; set; }
 }
---
+++ ASP.NET MVC Core 6/src/sql/Script.sql
@@ -0,0 +1,137 @@
+﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+    "MigrationId" TEXT NOT NULL CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY,
+    "ProductVersion" TEXT NOT NULL
+);
+
+BEGIN TRANSACTION;
+
+CREATE TABLE "AspNetRoles" (
+    "Id" TEXT NOT NULL CONSTRAINT "PK_AspNetRoles" PRIMARY KEY,
+    "Name" TEXT NULL,
+    "NormalizedName" TEXT NULL,
+    "ConcurrencyStamp" TEXT NULL
+);
+
+CREATE TABLE "AspNetUsers" (
+    "Id" TEXT NOT NULL CONSTRAINT "PK_AspNetUsers" PRIMARY KEY,
+    "UserName" TEXT NULL,
+    "NormalizedUserName" TEXT NULL,
+    "Email" TEXT NULL,
+    "NormalizedEmail" TEXT NULL,
+    "EmailConfirmed" INTEGER NOT NULL,
+    "PasswordHash" TEXT NULL,
+    "SecurityStamp" TEXT NULL,
+    "ConcurrencyStamp" TEXT NULL,
+    "PhoneNumber" TEXT NULL,
+    "PhoneNumberConfirmed" INTEGER NOT NULL,
+    "TwoFactorEnabled" INTEGER NOT NULL,
+    "LockoutEnd" TEXT NULL,
+    "LockoutEnabled" INTEGER NOT NULL,
+    "AccessFailedCount" INTEGER NOT NULL
+);
+
+CREATE TABLE "AspNetRoleClaims" (
+    "Id" INTEGER NOT NULL CONSTRAINT "PK_AspNetRoleClaims" PRIMARY KEY AUTOINCREMENT,
+    "RoleId" TEXT NOT NULL,
+    "ClaimType" TEXT NULL,
+    "ClaimValue" TEXT NULL,
+    CONSTRAINT "FK_AspNetRoleClaims_AspNetRoles_RoleId" FOREIGN KEY ("RoleId") REFERENCES "AspNetRoles" ("Id") ON DELETE CASCADE
+);
+
+CREATE TABLE "AspNetUserClaims" (
+    "Id" INTEGER NOT NULL CONSTRAINT "PK_AspNetUserClaims" PRIMARY KEY AUTOINCREMENT,
+    "UserId" TEXT NOT NULL,
+    "ClaimType" TEXT NULL,
+    "ClaimValue" TEXT NULL,
+    CONSTRAINT "FK_AspNetUserClaims_AspNetUsers_UserId" FOREIGN KEY ("UserId") REFERENCES "AspNetUsers" ("Id") ON DELETE CASCADE
+);
+
+CREATE TABLE "AspNetUserLogins" (
+    "LoginProvider" TEXT NOT NULL,
+    "ProviderKey" TEXT NOT NULL,
+    "ProviderDisplayName" TEXT NULL,
+    "UserId" TEXT NOT NULL,
+    CONSTRAINT "PK_AspNetUserLogins" PRIMARY KEY ("LoginProvider", "ProviderKey"),
+    CONSTRAINT "FK_AspNetUserLogins_AspNetUsers_UserId" FOREIGN KEY ("UserId") REFERENCES "AspNetUsers" ("Id") ON DELETE CASCADE
+);
+
+CREATE TABLE "AspNetUserRoles" (
+    "UserId" TEXT NOT NULL,
+    "RoleId" TEXT NOT NULL,
+    CONSTRAINT "PK_AspNetUserRoles" PRIMARY KEY ("UserId", "RoleId"),
+    CONSTRAINT "FK_AspNetUserRoles_AspNetRoles_RoleId" FOREIGN KEY ("RoleId") REFERENCES "AspNetRoles" ("Id") ON DELETE CASCADE,
+    CONSTRAINT "FK_AspNetUserRoles_AspNetUsers_UserId" FOREIGN KEY ("UserId") REFERENCES "AspNetUsers" ("Id") ON DELETE CASCADE
+);
+
+CREATE TABLE "AspNetUserTokens" (
+    "UserId" TEXT NOT NULL,
+    "LoginProvider" TEXT NOT NULL,
+    "Name" TEXT NOT NULL,
+    "Value" TEXT NULL,
+    CONSTRAINT "PK_AspNetUserTokens" PRIMARY KEY ("UserId", "LoginProvider", "Name"),
+    CONSTRAINT "FK_AspNetUserTokens_AspNetUsers_UserId" FOREIGN KEY ("UserId") REFERENCES "AspNetUsers" ("Id") ON DELETE CASCADE
+);
+
+CREATE INDEX "IX_AspNetRoleClaims_RoleId" ON "AspNetRoleClaims" ("RoleId");
+
+CREATE UNIQUE INDEX "RoleNameIndex" ON "AspNetRoles" ("NormalizedName");
+
+CREATE INDEX "IX_AspNetUserClaims_UserId" ON "AspNetUserClaims" ("UserId");
+
+CREATE INDEX "IX_AspNetUserLogins_UserId" ON "AspNetUserLogins" ("UserId");
+
+CREATE INDEX "IX_AspNetUserRoles_RoleId" ON "AspNetUserRoles" ("RoleId");
+
+CREATE INDEX "EmailIndex" ON "AspNetUsers" ("NormalizedEmail");
+
+CREATE UNIQUE INDEX "UserNameIndex" ON "AspNetUsers" ("NormalizedUserName");
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('00000000000000_CreateIdentitySchema', '6.0.1');
+
+COMMIT;
+
+BEGIN TRANSACTION;
+
+CREATE TABLE "Fornecedores" (
+    "Id" TEXT NOT NULL CONSTRAINT "PK_Fornecedores" PRIMARY KEY,
+    "Nome" TEXT NOT NULL,
+    "Documento" TEXT NOT NULL,
+    "TipoFornecedor" INTEGER NOT NULL,
+    "Ativo" INTEGER NOT NULL
+);
+
+CREATE TABLE "Enderecos" (
+    "Id" TEXT NOT NULL CONSTRAINT "PK_Enderecos" PRIMARY KEY,
+    "FornecedorId" TEXT NOT NULL,
+    "Logradouro" TEXT NOT NULL,
+    "Numero" TEXT NOT NULL,
+    "Complemento" TEXT NULL,
+    "Cep" TEXT NOT NULL,
+    "Bairro" TEXT NOT NULL,
+    "Cidade" TEXT NOT NULL,
+    "Estado" TEXT NOT NULL,
+    CONSTRAINT "FK_Enderecos_Fornecedores_FornecedorId" FOREIGN KEY ("FornecedorId") REFERENCES "Fornecedores" ("Id") ON DELETE CASCADE
+);
+
+CREATE TABLE "Produtos" (
+    "Id" TEXT NOT NULL CONSTRAINT "PK_Produtos" PRIMARY KEY,
+    "FornecedorId" TEXT NOT NULL,
+    "Nome" TEXT NOT NULL,
+    "Descricao" TEXT NOT NULL,
+    "Imagem" TEXT NOT NULL,
+    "Valor" TEXT NOT NULL,
+    "DataCadastro" TEXT NOT NULL,
+    "Ativo" INTEGER NOT NULL,
+    CONSTRAINT "FK_Produtos_Fornecedores_FornecedorId" FOREIGN KEY ("FornecedorId") REFERENCES "Fornecedores" ("Id") ON DELETE CASCADE
+);
+
+CREATE UNIQUE INDEX "IX_Enderecos_FornecedorId" ON "Enderecos" ("FornecedorId");
+
+CREATE INDEX "IX_Produtos_FornecedorId" ON "Produtos" ("FornecedorId");
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20220120235257_InitialSystem', '6.0.1');
+
+COMMIT;
+
